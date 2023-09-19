8823d4eb9fadd42e7263a81c755718d664e96df0
Camille HBP <11173924+CamilleHbp@users.noreply.github.com>
2023-09-19 16:36:10 +0200

    fix: disable base rule for no-shadow and enable typescript

--- .eslintrc.ts.js
+++ .eslintrc.ts.js
@@ -18,6 +18,10 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     // '@typescript-eslint/no-explicit-any': ['error', { fixToUnknown: true, ignoreRestArgs: true }],
     '@typescript-eslint/type-annotation-spacing': 'error',
+    // Note: must disable the base rule as it can report incorrect errors
+    // See https://typescript-eslint.io/rules/no-shadow/#how-to-use
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
     /* --------------------------------- Fastify -------------------------------- */
     // INFO: no-floating-promises requires all thenable to be handled, even when they are not real promises. Using the void qualifier  evaluates the expression then returns undefined. See https://github.com/fastify/fastify/discussions/3849 and https://github.com/typescript-eslint/typescript-eslint/issues/2640
     'no-void': 'warn',
